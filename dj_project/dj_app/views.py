420199b5251842ea1b78ddcb8c655671ab35e048
Muhammad Hazaifa <hazaifa.awan@gmail.com>
2020-12-23 18:23:23 +0500

    pages and urls update

--- dj_project/dj_app/views.py
+++ dj_project/dj_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index (me):
+    return HttpResponse ("<h> index page<h1>")
+def newpage (firstpage):
+    return HttpResponse ("<h1> new page<h1>")
